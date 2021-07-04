04a18f2102f6ca6bad6836fcdda3ed74f2dcf219
Yash Singh <yscatchyash@gmail.com>
2021-07-04 19:23:09 +0530

    Create version.h

---
+++ version.h
@@ -0,0 +1,5 @@
+#ifndef VERSION_H_
+    #define VERSION_H_
+    void _version(); /* prints the  C/C++ dialect
+                        the compiler intends to comply with */
+#endif /* VERSION_H_ */
